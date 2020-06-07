9a9b5f3a285d81116f2975c1a0d679ff71bc5937
MerMerYEE <65539542+MerMerYEE@users.noreply.github.com>
2020-06-07 15:45:01 +0900

    Update bot.py
    ㅁㄴㅇㄹ

--- bot.py
+++ bot.py
@@ -226,7 +226,7 @@ async def help(ctx):
     embed=discord.Embed(color=0xff00, title="명령어")
     embed.set_footer(text=client.get_user(444363545635848193).name, icon_url=client.get_user(444363545635848193).avatar_url)
     embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/704619025258512444.png?size=1024")
-    embed.add_field(name = '관리 명령어', value = 'ban, unban, mute, unmute (뮤트기능은 뮤트 라는 역할이 있어야 실행됨)')
+    embed.add_field(name = '관리 명령어', value = '밴, 언밴, 뮤트, unmute (뮤트기능은 뮤트 라는 역할이 있어야 실행됨, 밴 명령어는 Commander이라는 역할을 착용하고 있어야 가능함)')
     embed.add_field(name = '잡 명령어', value = '핑(ping), 주인, help, 명령어, 도움말, command, 타이머, 제작, 거꾸로, 사랑해')
     embed.add_field(name = '링크 명령어', value = '메이플(전적), maple, 롤(전적), lol(전적), 트위치(찾기), 오버워치(전적), overwatch(전적)')
     embed.add_field(name = '띵크', value = "띵크")
@@ -237,7 +237,7 @@ async def 도움말(ctx):
     embed=discord.Embed(color=0xff00, title="명령어")
     embed.set_footer(text=client.get_user(444363545635848193).name, icon_url=client.get_user(444363545635848193).avatar_url)
     embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/704619025258512444.png?size=1024")
-    embed.add_field(name = '관리 명령어', value = 'ban, unban, mute, unmute (뮤트기능은 뮤트 라는 역할이 있어야 실행됨)')
+    embed.add_field(name = '관리 명령어', value = '밴, 언밴, 뮤트, unmute (뮤트기능은 뮤트 라는 역할이 있어야 실행됨, 밴 명령어는 Commander이라는 역할을 착용하고 있어야 가능함)'
     embed.add_field(name = '잡 명령어', value = '핑(ping), 주인, help, 명령어, 도움말, command, 타이머, 제작, 거꾸로, 사랑해')
     embed.add_field(name = '링크 명령어', value = '메이플(전적), maple, 롤(전적), lol(전적), 트위치(찾기), 오버워치(전적), overwatch(전적)')
     embed.add_field(name = '띵크', value = "띵크")
@@ -248,7 +248,7 @@ async def 명령어(ctx):
     embed=discord.Embed(color=0xff00, title="명령어")
     embed.set_footer(text=client.get_user(444363545635848193).name, icon_url=client.get_user(444363545635848193).avatar_url)
     embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/704619025258512444.png?size=1024")
-    embed.add_field(name = '관리 명령어', value = 'ban, unban, mute, unmute (뮤트기능은 뮤트 라는 역할이 있어야 실행됨)')
+    embed.add_field(name = '관리 명령어', value = '밴, 언밴, 뮤트, unmute (뮤트기능은 뮤트 라는 역할이 있어야 실행됨, 밴 명령어는 Commander이라는 역할을 착용하고 있어야 가능함)'
     embed.add_field(name = '잡 명령어', value = '핑(ping), 주인, help, 명령어, 도움말, command, 타이머, 제작, 거꾸로, 사랑해')
     embed.add_field(name = '링크 명령어', value = '메이플(전적), maple, 롤(전적), lol(전적), 트위치(찾기), 오버워치(전적), overwatch(전적)')
     embed.add_field(name = '띵크', value = "띵크")
