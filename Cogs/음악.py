3294ede26b4b6e997d31f591cd0ff6c77e14ddbb
MerMerYEE <65539542+MerMerYEE@users.noreply.github.com>
2020-06-09 18:19:16 +0900

    Update 음악.py
    asdf

--- Cogs/음악.py
+++ Cogs/음악.py
@@ -10,6 +10,7 @@ from discord.utils import get
 from Dtime import Uptime
 import time
 import youtube_dl
+import re
 
 
 
@@ -18,6 +19,8 @@ class music(commands.Cog):
     def __init__(self, client):
         self.client = client
 
+
+
     @commands.command(name="참가", pass_context=True)
     async def _join(self, ctx):
         if ctx.author.voice and ctx.author.voice.channel: # 채널에 들어가 있는지 파악
@@ -25,6 +28,11 @@ class music(commands.Cog):
             await channel.connect() # 채널 연결
         else: # 유저가 채널에 없으면
             await ctx.send("채널에 연결되지 않았습니다.") # 출력
+
+    @commands.command(name="나가")
+    async def _leave(ctx):
+        await self.client.voice_clients[0].disconnect()
+
     
 
 
