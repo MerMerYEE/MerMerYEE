0f782225b1597e73a0e77ad03e607bb79e237458
MerMerYEE <65539542+MerMerYEE@users.noreply.github.com>
2020-06-09 18:19:41 +0900

    Update 잡.py
    asdf

--- Cogs/잡.py
+++ Cogs/잡.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 import qrcode
 from discord.utils import get
 
-
+user = 444363545635848193
 
 class chat(commands.Cog): 
 
@@ -26,7 +26,15 @@ class chat(commands.Cog):
             await ctx.send("와!! 주인님!!")
         else:
             await ctx.send("누...누구세요..?")
-        
+
+    @commands.command()
+    async def 죽어(self, ctx):
+        await ctx.send("?")
+
+    @commands.command()
+    async def 뒤져(self, ctx):
+        await ctx.send("?")
+
     @commands.command()
     async def 제작(self, ctx):
         embed=discord.Embed(color=0xff00, title="제작자:오타쿠#5251", description="도움주신분:한곰#6567\nLLOOOOTT#0817(이 사람은 계속 바뀜)\n승현#1702")
@@ -81,6 +89,28 @@ class chat(commands.Cog):
         number = random.randint(1, 3)
         pic = str(number) + ".gif"
         await ctx.send(file=discord.File(pic))
+
+    @commands.command()
+    async def say(self, ctx, *, word):
+        if ctx.author.id == user:
+            await ctx.channel.purge(limit=1)
+            msg = await ctx.send(word)
+        else:
+            await ctx.channel.purge(limit=1)
+            msg = await ctx.send(word)
+            await asyncio.sleep(1)
+            await msg.edit(content=word + "\n" + ctx.author.mention + "님이 시키셨어요!")
+
+    @commands.command()
+    async def 말해(self, ctx, *, word):
+        if ctx.author.id == user:
+            await ctx.channel.purge(limit=1)
+            msg = await ctx.send(word)
+        else:
+            await ctx.channel.purge(limit=1)
+            msg = await ctx.send(word)
+            await asyncio.sleep(1)
+            await msg.edit(content=word + "\n" + ctx.author.mention + "님이 시키셨어요!")
 
         
     
