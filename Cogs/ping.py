ea02fed973de108185ddbc608efb29cceb402cf3
MerMerYEE <65539542+MerMerYEE@users.noreply.github.com>
2020-06-11 08:52:36 +0900

    Update ping.py
    ASDF

--- Cogs/ping.py
+++ Cogs/ping.py
@@ -11,6 +11,7 @@ from Dtime import Uptime
 import time
 import typing
 
+num = random.randint(1, 5)
 
 class ping(commands.Cog):
 
@@ -18,16 +19,31 @@ class ping(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def 봇정보(ctx):
+    async def 봇정보(self, ctx):
         await ctx.send(f"{int(self.client.latency *1000)}ms이야!")
+        if num == 1:
+            embed=discord.Embed(color=0xff00, description = "[미도리야가 마음에 드신다면 :heart:를 눌러주세요!](http://koreanbots.dev/bots/704619025258512444)")
+            await ctx.send(embed=embed)
+        else:
+            return
 
     @commands.command()
-    async def 핑(ctx):
+    async def 핑(self, ctx):
         await ctx.send(f"{int(self.client.latency *1000)}ms이야!")
+        if num == 1:
+            embed=discord.Embed(color=0xff00, description = "[미도리야가 마음에 드신다면 :heart:를 눌러주세요!](http://koreanbots.dev/bots/704619025258512444)")
+            await ctx.send(embed=embed)
+        else:
+            return
 
     @commands.command()
-    async def ping(ctx):
+    async def ping(self, ctx):
         await ctx.send(f"{int(self.client.latency *1000)}ms이야!")
+        if num == 1:
+            embed=discord.Embed(color=0xff00, description = "[미도리야가 마음에 드신다면 :heart:를 눌러주세요!](http://koreanbots.dev/bots/704619025258512444)")
+            await ctx.send(embed=embed)
+        else:
+            return
 
     
 
