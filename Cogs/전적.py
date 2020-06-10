5ce2660319b48cb5a446aed7ef8d7f1bd78855b6
MerMerYEE <65539542+MerMerYEE@users.noreply.github.com>
2020-06-11 08:56:19 +0900

    Update 전적.py
    asdf

--- Cogs/전적.py
+++ Cogs/전적.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 import qrcode
 from discord.utils import get
 
-
+num = random.randint(1, 5)
 
 class 전적(commands.Cog): 
 
@@ -18,34 +18,74 @@ class 전적(commands.Cog):
     @commands.command()
     async def 오버워치(self, ctx, over):
         await ctx.send("https://overwatch.op.gg/search?playerName=" + over)
+        if num == 1:
+            embed=discord.Embed(color=0xff00, description = "[미도리야가 마음에 드신다면 :heart:를 눌러주세요!](http://koreanbots.dev/bots/704619025258512444)")
+            await ctx.send(embed=embed)
+        else:
+            return
 
     @commands.command()
     async def overwatch(self, ctx, over2):
         await ctx.send("https://overwatch.op.gg/search?playerName=" + over2)
+        if num == 1:
+            embed=discord.Embed(color=0xff00, description = "[미도리야가 마음에 드신다면 :heart:를 눌러주세요!](http://koreanbots.dev/bots/704619025258512444)")
+            await ctx.send(embed=embed)
+        else:
+            return
 
     @commands.command()
     async def 트위치(self, ctx, twitch):
         await ctx.send("https://m.twitch.tv/"+twitch)
+        if num == 1:
+            embed=discord.Embed(color=0xff00, description = "[미도리야가 마음에 드신다면 :heart:를 눌러주세요!](http://koreanbots.dev/bots/704619025258512444)")
+            await ctx.send(embed=embed)
+        else:
+            return
 
     @commands.command()
     async def twitch(self, ctx, twitch2):
         await ctx.send("https://m.twitch.tv/"+twitch2)
+        if num == 1:
+            embed=discord.Embed(color=0xff00, description = "[미도리야가 마음에 드신다면 :heart:를 눌러주세요!](http://koreanbots.dev/bots/704619025258512444)")
+            await ctx.send(embed=embed)
+        else:
+            return
 
     @commands.command()
     async def 메이플(self, ctx, maple1):
         await ctx.send("https://maple.gg/u/"+maple1)
+        if num == 1:
+            embed=discord.Embed(color=0xff00, description = "[미도리야가 마음에 드신다면 :heart:를 눌러주세요!](http://koreanbots.dev/bots/704619025258512444)")
+            await ctx.send(embed=embed)
+        else:
+            return
 
     @commands.command()
     async def maple(self, ctx, maple2):
         await ctx.send("https://maple.gg/u/"+maple2)
+        if num == 1:
+            embed=discord.Embed(color=0xff00, description = "[미도리야가 마음에 드신다면 :heart:를 눌러주세요!](http://koreanbots.dev/bots/704619025258512444)")
+            await ctx.send(embed=embed)
+        else:
+            return
 
     @commands.command()
     async def lol(self, ctx, lol):
         await ctx.send("https://www.op.gg/summoner/userName="+lol)
+        if num == 1:
+            embed=discord.Embed(color=0xff00, description = "[미도리야가 마음에 드신다면 :heart:를 눌러주세요!](http://koreanbots.dev/bots/704619025258512444)")
+            await ctx.send(embed=embed)
+        else:
+            return
 
     @commands.command()
     async def 롤(self, ctx, lol2):
         await ctx.send("https://www.op.gg/summoner/userName="+lol2)
+        if num == 1:
+            embed=discord.Embed(color=0xff00, description = "[미도리야가 마음에 드신다면 :heart:를 눌러주세요!](http://koreanbots.dev/bots/704619025258512444)")
+            await ctx.send(embed=embed)
+        else:
+            return
     
 
 def setup(client): 
