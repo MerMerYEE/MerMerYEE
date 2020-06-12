e9b5b4f5e73d4b6aaf8064bfad4a8029784b3f1b
MerMerYEE <65539542+MerMerYEE@users.noreply.github.com>
2020-06-12 09:58:18 +0900

    Update help.py
    asdf

--- Cogs/help.py
+++ Cogs/help.py
@@ -19,7 +19,7 @@ class help(commands.Cog):
         embed.add_field(name="유틸", value="타이머, 주사위, roll, 아바타, avatar, 유저정보, userinfo, 홀짝(미완이라 숫자만 되요!), 업타임, ping, 핑, 봇정보")
         embed.add_field(name="밈", value="관짝, 관짝춤")
         embed.add_field(name="관리", value="밴, 언밴, 킥, 추방, 뮤트(뮤트라는 역할이 있어야 해요!), 언뮤트(뮤트라는 역할이 있어야 해요!)")
-        embed.add_field(name="띵킹눈, 띵킹빵, 띵킹버거, 띵킹박수, 띵킹가지, 띵킹피젯, 띵킹물고기, 띵킹하드, 띵킹인터넷, 띵킹레몬, 띵킹비정상")
+        embed.add_field(name="띵킹눈" value="띵킹빵, 띵킹버거, 띵킹박수, 띵킹가지, 띵킹피젯, 띵킹물고기, 띵킹하드, 띵킹인터넷, 띵킹레몬, 띵킹비정상")
         embed.add_field(name="음악", value="play(p, ㅔ), connect(join)<--비상용, pause(일시중지), skip(s, ㄴ), queue(q, playlist),  now_playing(np, current, playing),volume(vol), stop")
         await ctx.send(ctx.author.mention + "DM으로 도움말을 보냈어요!")
         await msg.send(embed=embed)
