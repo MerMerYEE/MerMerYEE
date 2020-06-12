b678f51fea67e1f2901fa184d07678cd91042dd4
MerMerYEE <65539542+MerMerYEE@users.noreply.github.com>
2020-06-12 09:27:30 +0900

    Add files via upload
    ㅁㄴㅇㄹ

---
+++ Cogs/help.py
@@ -0,0 +1,63 @@
+import discord
+from discord.ext import commands
+
+class help(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def help(self, ctx)
+        channel = await ctx.author.create_dm()
+        embed = discord.Embed(title="*명령어!*", color=0xff00)
+        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/704619025258512444/6b4cf3a72bb22ad2726a46f6a508c5ad.webp?size=1024")
+        embed.add_field(name="잡 명령어", value="사랑해, 주인, 죽어, 제작, 띵크, 띵킹, 초대, invite say, 말해, 때려")
+        embed.add_field(name="밈", value="관짝, 관짝춤")
+        embed.add_field(name="유틸", value="타이머, 주사위, roll, 아바타, avatar, 유저정보, userinfo, 홀짝(미완이라 숫자만 되요!), 업타임, ping, 핑, 봇정보")
+        embed.add_field(name="밈", value="관짝, 관짝춤")
+        embed.add_field(name="관리", value="밴, 언밴, 킥, 추방, 뮤트(뮤트라는 역할이 있어야 해요!), 언뮤트(뮤트라는 역할이 있어야 해요!)")
+        embed.add_field(name="띵킹눈, 띵킹빵, 띵킹버거, 띵킹박수, 띵킹가지, 띵킹피젯, 띵킹물고기, 띵킹하드, 띵킹인터넷, 띵킹레몬, 띵킹비정상")
+        embed.add_field(name="음악", value="play(p, ㅔ), connect(join)<--비상용, pause(일시중지), skip(s, ㄴ), queue(q, playlist),  now_playing(np, current, playing),volume(vol), stop")
+        await channel.send(embed=embed)
+
+    @commands.command()
+    async def 도움말(self, ctx)
+        channel = await ctx.author.create_dm()
+        embed = discord.Embed(title="*도움말!*", color=0xff00)
+        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/704619025258512444/6b4cf3a72bb22ad2726a46f6a508c5ad.webp?size=1024")
+        embed.add_field(name="잡 명령어", value="사랑해, 주인, 죽어, 제작, 띵크, 띵킹, 초대, invite say, 말해, 때려")
+        embed.add_field(name="밈", value="관짝, 관짝춤")
+        embed.add_field(name="유틸", value="타이머, 주사위, roll, 아바타, avatar, 유저정보, userinfo, 홀짝(미완이라 숫자만 되요!), 업타임, ping, 핑, 봇정보")
+        embed.add_field(name="밈", value="관짝, 관짝춤")
+        embed.add_field(name="관리", value="밴, 언밴, 킥, 추방, 뮤트(뮤트라는 역할이 있어야 해요!), 언뮤트(뮤트라는 역할이 있어야 해요!)")
+        embed.add_field(name="띵킹눈, 띵킹빵, 띵킹버거, 띵킹박수, 띵킹가지, 띵킹피젯, 띵킹물고기, 띵킹하드, 띵킹인터넷, 띵킹레몬, 띵킹비정상")
+        embed.add_field(name="음악", value="play(p, ㅔ), connect(join)<--비상용, pause(일시중지), skip(s, ㄴ), queue(q, playlist),  now_playing(np, current, playing),volume(vol), stop")
+        await channel.send(embed=embed)
+
+    @commands.command()
+    async def 도움(self, ctx)
+        channel = await ctx.author.create_dm()
+        embed = discord.Embed(title="*도움말!*", color=0xff00)
+        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/704619025258512444/6b4cf3a72bb22ad2726a46f6a508c5ad.webp?size=1024")
+        embed.add_field(name="잡 명령어", value="사랑해, 주인, 죽어, 제작, 띵크, 띵킹, 초대, invite say, 말해, 때려")
+        embed.add_field(name="밈", value="관짝, 관짝춤")
+        embed.add_field(name="유틸", value="타이머, 주사위, roll, 아바타, avatar, 유저정보, userinfo, 홀짝(미완이라 숫자만 되요!), 업타임, ping, 핑, 봇정보")
+        embed.add_field(name="밈", value="관짝, 관짝춤")
+        embed.add_field(name="관리", value="밴, 언밴, 킥, 추방, 뮤트(뮤트라는 역할이 있어야 해요!), 언뮤트(뮤트라는 역할이 있어야 해요!)")
+        embed.add_field(name="띵킹눈, 띵킹빵, 띵킹버거, 띵킹박수, 띵킹가지, 띵킹피젯, 띵킹물고기, 띵킹하드, 띵킹인터넷, 띵킹레몬, 띵킹비정상")
+        embed.add_field(name="음악", value="play(p, ㅔ), connect(join)<--비상용, pause(일시중지), skip(s, ㄴ), queue(q, playlist),  now_playing(np, current, playing),volume(vol), stop")
+        await channel.send(embed=embed)
+
+    @commands.command()
+    async def cmds(self, ctx)
+        channel = await ctx.author.create_dm()
+        embed = discord.Embed(title="*커맨드!!*", color=0xff00)
+        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/704619025258512444/6b4cf3a72bb22ad2726a46f6a508c5ad.webp?size=1024")
+        embed.add_field(name="잡 명령어", value="사랑해, 주인, 죽어, 제작, 띵크, 띵킹, 초대, invite say, 말해, 때려")
+        embed.add_field(name="밈", value="관짝, 관짝춤")
+        embed.add_field(name="유틸", value="타이머, 주사위, roll, 아바타, avatar, 유저정보, userinfo, 홀짝(미완이라 숫자만 되요!), 업타임, ping, 핑, 봇정보")
+        embed.add_field(name="밈", value="관짝, 관짝춤")
+        embed.add_field(name="관리", value="밴, 언밴, 킥, 추방, 뮤트(뮤트라는 역할이 있어야 해요!), 언뮤트(뮤트라는 역할이 있어야 해요!)")
+        embed.add_field(name="띵킹눈, 띵킹빵, 띵킹버거, 띵킹박수, 띵킹가지, 띵킹피젯, 띵킹물고기, 띵킹하드, 띵킹인터넷, 띵킹레몬, 띵킹비정상")
+        embed.add_field(name="음악", value="play(p, ㅔ), connect(join)<--비상용, pause(일시중지), skip(s, ㄴ), queue(q, playlist),  now_playing(np, current, playing),volume(vol), stop")
+        await channel.send(embed=embed)
